83920442cac68218c874331dd8a6bef68962e5f0
Documenter.jl <documenter@juliadocs.github.io>
2021-06-09 17:35:02 +0000

    build based on 9f1600a

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Hello-world-PostgresORM","page":"Home","title":"Hello world PostgresORM","text":"","category":"section"}]
+[{"location":"#PostgresORM.jl","page":"Home","title":"PostgresORM.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the PostgresORM.jl documentation!","category":"page"},{"location":"","page":"Home","title":"Home","text":"This resource aims to teach you everything you need to know to get up and running with Object Relational Mapping between a Julia project and a PostgreSQL database  using PostgresORM.jl package.","category":"page"}]
 }
